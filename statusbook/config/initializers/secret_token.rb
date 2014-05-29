9aed6d3369d2d65a9bf854a42ff4fe696546302d
prank7 <prashant.abhishek7g@gmail.com>
2014-05-29 17:21:31 +0530

    statusbook; first project in rails

---
+++ statusbook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mygod::Application.config.secret_token = '298f2bcdaef245df1d505677b8bc63940c8a662f0dfa3267da0cd3b72c8fd156a61d5915670ac02d6ea985cc2d51193ea738fd0fcf9fc8dcbb9e066d46f3247f'
