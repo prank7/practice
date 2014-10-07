64394c78463eaa126b493eaf6241d03fd595ca2a
prank7 <prashant.abhishek7g@gmail.com>
2014-10-07 23:55:54 +0530

    kata 32 & 33

---
+++ codewars/kata33.rb
@@ -0,0 +1,7 @@
+#Write a method that takes one argument as name and then greets that name,
+# capitalized and ends with an exclamation point.
+
+#My solution - best practice
+def greet(name)
+  "Hello #{name.capitalize}!"
+end
